7e354f3b8be92bb05184d30b805c6df0d1c9fe04
Jixian Wang <jixian@pickatale.com>
2014-01-16 11:08:07 +0700

    update ui and app

--- app/modules/account/models/Account.js
+++ app/modules/account/models/Account.js
@@ -5,37 +5,48 @@ module.exports = function(app)
     var schema = new mongoose.Schema({
         _id: mongoose.Schema.Types.ObjectId,
         name: String,
-        address:{            
-            country: String,
-            province: String,
-            city: String,
-            street: String,
-            zipcode: String
+        info:{
+            address:{            
+                country: String,
+                province: String,
+                city: String,
+                street: String,
+                zipcode: String
+            },
+            phone:{
+                countryCode: String,
+                areaCode:   Number,
+                number:     Number
+            }
         },
-        phone:{
-            countryCode: String,
-            areaCode:   Number,
-            number:     Number
-        },
-        administrators:[{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'cubi.user.User'
-        }],        
         users:[{
-        	type: mongoose.Schema.Types.ObjectId,
-            ref: 'cubi.user.User'
-        }],
-        creator:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'cubi.user.User'
-        },
+        	userId:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'cubi.user.User'
+            },
+            role:{
+                type: String,
+                enum: ['administrator','member'],
+                default: 'member'
+            }
+        }],        
         contacts:[{
             type: mongoose.Schema.Types.ObjectId,
             ref: 'cubi.contact.Contact'
-        }]
+        }],
+        creator:{
+            id:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'cubi.user.User'
+            },
+            timestamp: {
+                type: Date,
+                default: Date.now
+            }
+        }       
     },{
         versionKey: false,
-        collection: 'cubi-account'
+        collection: 'cubi_account'
     });
     return mongoose.model('cubi.account.Account', schema);
 }
