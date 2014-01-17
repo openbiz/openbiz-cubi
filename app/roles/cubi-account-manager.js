510798a0c80dd138c190484715fe74808b384e7e
Jixian Wang <jixian@pickatale.com>
2014-01-17 22:51:02 +0700

    update ui and app

---
+++ app/roles/cubi-account-manager.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = 
+[
+	"cubi-account-manage"	//if user CREATE a company account, at the monment he will has this role
+];
---
+++ app/roles/cubi-account-member.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = 
+[
+	"cubi-account-access"	//if user JOIN a company account, at the monment he will has this role
+];
