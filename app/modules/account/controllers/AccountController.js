aff3976d386f945d72c7897963fdda3dbdb2c432
Jixian Wang <jixian@pickatale.com>
2014-03-06 18:54:09 +0800

    update

--- app/modules/account/controllers/AccountController.js
+++ app/modules/account/controllers/AccountController.js
@@ -135,7 +135,8 @@ module.exports = function(app){
 			});
 		},
 		getInvitationTokens:function(req,res){
-			res.json(200,req.user.account.invitations);
+			var results = app.openbiz.services.ArrayPaginator(req.user.account.invitations,req.query);
+			res.json(200,results);
 		},
 		getInvitationToken:function(req,res){
 			res.json(200,req.user.account.invitations.id(req.params.id.toUpperCase()));
--- ui/modules/account/models/InvitationCollection.js
+++ ui/modules/account/models/InvitationCollection.js
@@ -1,7 +1,12 @@
 "use strict";
 define(['./Invitation'],function(Invitation){
-    return Backbone.Collection.extend({
+    return Backbone.PageableCollection.extend({
         model: Invitation,
-        url: openbiz.apps.cubi.appUrl ? openbiz.apps.cubi.appUrl+'/account/invitations' : null,        
+        url: openbiz.apps.cubi.appUrl ? openbiz.apps.cubi.appUrl+'/account/invitations' : null,    
+        state: {
+		    pageSize: 3,
+		    sortKey: "expiredDate",
+		    order: 1
+		}
     });
 });
