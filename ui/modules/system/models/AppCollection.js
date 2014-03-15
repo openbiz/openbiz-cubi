32da09f090e443ce679e3a6edd40306e233df7ca
Jixian Wang <jixian@pickatale.com>
2014-03-15 20:43:19 +0800

    restored appcollection

--- ui/modules/system/models/AppCollection.js
+++ ui/modules/system/models/AppCollection.js
@@ -1,9 +1,8 @@
 "use strict";
 define(['./App'],function(App){
-    return Backbone.PageableCollection.extend({
+    return Backbone.Collection.extend({
         model: App,
         url: openbiz.apps.cubi.appUrl ? openbiz.apps.cubi.appUrl+'/apps' : null,
-<<<<<<< HEAD
         sync:function(method,model,options){
             switch(method){
                 case 'read':
@@ -16,8 +15,8 @@ define(['./App'],function(App){
                                     baseUrl:app.baseUrl,
                                     context:"appCollection"+app.name,
                                     paths:{
-                                        'i18n'	: openbiz.baseUrl+'/vendor/require/plugins/i18n',
-                                        'text'	: openbiz.baseUrl+'/vendor/require/plugins/text'
+                                        'i18n'  : openbiz.baseUrl+'/vendor/require/plugins/i18n',
+                                        'text'  : openbiz.baseUrl+'/vendor/require/plugins/text'
                                     },
                                     config:{
                                         i18n:{
@@ -41,7 +40,7 @@ define(['./App'],function(App){
                                             }                 
                                             success(returnData,status,xhr);
                                         }
-                                    } 
+                                    }                                    
                                     appRequire(['./menu/main'],function(menu){
                                         app.menu = new menu();
                                         callback();
@@ -53,62 +52,6 @@ define(['./App'],function(App){
                     break;
             }
             Backbone.sync.apply(this,arguments);
-=======
-        state: {
-            pageSize: 10,
-            sortKey: "name",
-            order: 1
->>>>>>> bf3f33902e0288c68decbaca379993e2312f5c5c
         }
-//        sync:function(method,model,options){
-//            switch(method){
-//                case 'read':
-//                    var success = options.success;
-//                    options.success = function(data,status,xhr){
-//                        if(_.isArray( data ) && data.length){
-//                            var loadedLocales = {},loadedLocalesCounter=0;
-//                            data.forEach(function(app){
-//                                var appRequire = require.config({
-//                                    baseUrl:app.baseUrl,
-//                                    context:"appCollection"+app.name,
-//                                    paths:{
-//                                        'i18n'	: openbiz.baseUrl+'/vendor/require/plugins/i18n',
-//                                        'text'	: openbiz.baseUrl+'/vendor/require/plugins/text'
-//                                    },
-//                                    config:{
-//                                        i18n:{
-//                                            locale: 'zh-cn' //force to use this locale for test translation
-//                                        }
-//                                    }
-//                                });
-//                                app.require = appRequire;
-//                                appRequire(['i18n!./nls/locale'],function(locale){
-//                                    if(locale.hasOwnProperty('app')){
-//                                        app.locale = locale.app;
-//                                    }
-//                                    //load apps menu scripts
-//                                    var callback = function(){
-//                                        loadedLocales[app.name]=app;
-//                                        loadedLocalesCounter++;
-//                                        if(loadedLocalesCounter == data.length){
-//                                            var returnData=[];
-//                                            for(var i in data){
-//                                                returnData[i]=loadedLocales[data[i].name];
-//                                            }
-//                                            success(returnData,status,xhr);
-//                                        }
-//                                    }
-//                                    appRequire(['./menu/main'],function(menu){
-//                                        app.menu = new menu();
-//                                        callback();
-//                                    },callback);
-//                                });
-//                            })
-//                        }
-//                    };
-//                    break;
-//            }
-//            Backbone.sync.apply(this,arguments);
-//        }
     });
 });
