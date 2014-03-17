d4a6929216302dbaa992ca9ba798f78a92faadb6
Zhang Dong <zhangdongzdme@vip.qq.com>
2014-03-17 10:35:59 +0800

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,120 +1,123 @@
 var path = require('path');
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jsdoc: {
-      dist: {
-        src: [
-            'app/**/*.js'
-        ],
-        dest: 'doc'
-      }
-    },
-    jade: {
-      compile: {
-        options: {
-          pretty: false,
-          data: {
-            debug: false,
-          }
-        },
-        files: [
-          {
-            expand: true, 
-            cwd: 'ui/',
-            src: "**/*.jade",
-            dest: "ui/",
-            ext: ".html"
-          }
-        ]
-      }
-    },
-    requirejs: {
-      cubi: {
-        options: {
-          preserveLicenseComments:false,
-          baseUrl: "./ui",
-          useStrict: true,          
-          name: "main",
-          out: "ui/main.min.js",
-          paths:{
-            'i18n'    : 'vendor/require/plugins/i18n',
-            'text'    : 'vendor/require/plugins/text'            
-          },
-          shim:{
-            'modules/account/main':{
-              deps: [ 
-                      //models
-                      'modules/account/models/User', 
-                      'modules/account/models/UserCollection',
-                      'modules/account/models/Account',
-                      'modules/account/models/Invitation',
-                      'modules/account/models/InvitationCollection',
-                      //views
-                      'modules/account/views/ApplicationsView',
-                      'modules/account/views/MembersListView',
-                      'modules/account/views/MembersEditView',
-                      'modules/account/views/InvitationsListView',
-                      'modules/account/views/InvitationsNewView',
-                      'modules/account/views/InvitationsDetailView',
-                      'modules/account/views/ProfileView',
-                      'modules/account/views/BillingView'
-                    ]
-            },
-            'modules/system/main':{
-              deps: [ 
-                      //models
-                      'modules/system/models/App', 
-                      'modules/system/models/AppCollection',
-                      //views
-                      'modules/system/views/LayoutView',
-                      'modules/system/views/ContactRightView',
-                      'modules/system/views/MenuView',
-                      'modules/system/views/NavView',
-                      'modules/system/views/HeaderView',
-                    ]
-            },
-            'modules/user/main':{
-              deps: [ 
-                      //models
-                      'modules/user/models/User',         
-                      //views              
-                      'modules/user/views/DashboardView',
-                      'modules/user/views/ForgetPasswordView',
-                      'modules/user/views/LoginView',
-                      'modules/user/views/RegisterView'
-                    ]
-            },
-            'modules/me/main':{
-              deps: [ 
-                      //models
-                      'modules/me/models/Me',         
-                      //views              
-                      'modules/me/views/SetupWizardView',
-	                  'modules/me/views/UserProfileView'
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jsdoc: {
+			dist: {
+				src: [
+					'app/**/*.js'
+				],
+				dest: 'doc'
+			}
+		},
+		jade: {
+			compile: {
+				options: {
+					pretty: false,
+					data: {
+						debug: false,
+					}
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'ui/',
+						src: "**/*.jade",
+						dest: "ui/",
+						ext: ".html"
+					}
+				]
+			}
+		},
+		requirejs: {
+			cubi: {
+				options: {
+					preserveLicenseComments:false,
+					baseUrl: "./ui",
+					useStrict: true,
+					name: "main",
+					out: "ui/main.min.js",
+					paths:{
+						'i18n'    : 'vendor/require/plugins/i18n',
+						'text'    : 'vendor/require/plugins/text'
+					},
+					shim:{
+						'modules/account/main':{
+							deps: [
+								//models
+								'modules/account/models/User',
+								'modules/account/models/UserCollection',
+								'modules/account/models/Account',
+								'modules/account/models/Invitation',
+								'modules/account/models/InvitationCollection',
+								//views
+								'modules/account/views/ApplicationsDetailView',
+								'modules/account/views/ApplicationsListView',
+								'modules/account/views/ApplicationsView',
+								'modules/account/views/BillingView',
+								'modules/account/views/InvitationsDetailView',
+								'modules/account/views/InvitationsListView',
+								'modules/account/views/InvitationsNewPermissionView',
+								'modules/account/views/InvitationsNewView',
+								'modules/account/views/MembersEditView',
+								'modules/account/views/MembersListView',
+								'modules/account/views/ProfileView',
+							]
+						},
+						'modules/system/main':{
+							deps: [
+								//models
+								'modules/system/models/App',
+								'modules/system/models/AppCollection',
+								//views
+								'modules/system/views/LayoutView',
+								'modules/system/views/ContactRightView',
+								'modules/system/views/MenuView',
+								'modules/system/views/NavView',
+								'modules/system/views/HeaderView',
+							]
+						},
+						'modules/user/main':{
+							deps: [
+								//models
+								'modules/user/models/User',
+								//views
+								'modules/user/views/DashboardView',
+								'modules/user/views/ForgetPasswordView',
+								'modules/user/views/LoginView',
+								'modules/user/views/RegisterView'
+							]
+						},
+						'modules/me/main':{
+							deps: [
+								//models
+								'modules/me/models/Me',
+								//views
+								'modules/me/views/SetupWizardView',
+								'modules/me/views/UserProfileView'
 
-              ]
-            }
-          }
-        }
-      }
-    },
-    clean: {
-      html: {
-        src: [
-          'ui/**/*.html'
-        ]
-      }      
-    }
-  });
+							]
+						}
+					}
+				}
+			}
+		},
+		clean: {
+			html: {
+				src: [
+					'ui/**/*.html'
+				]
+			}
+		}
+	});
 
 
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['requirejs','jade','jsdoc']);
+	grunt.registerTask('default', ['build']);
+	grunt.registerTask('build', ['requirejs','jade','jsdoc']);
 };
