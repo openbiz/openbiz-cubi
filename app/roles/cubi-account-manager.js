974ee50974ec047e6b1bce0e2e29c8413df35a5c
Jixian Wang <jixian@pickatale.com>
2014-04-05 14:25:34 +0800

    update

--- app/roles/cubi-account-manager.js
+++ app/roles/cubi-account-manager.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = {
 	permissions:[
-		"cubi-account-manage"	//if user CREATE a company account, at the monment he will has this role
+		"cubi-account-manage",	//if user CREATE a company account, at the monment he will has this role,
+		"cubi-account-access"
 	],
 	isDefault: false
 }
